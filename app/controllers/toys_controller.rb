bf696bfe926600d85ee7bc90d24be6766aa9d102
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-06-28 16:52:50 -0400

    commit tree b15f8d84fce5e5f74ad9a38d8ea0e2ee70e9b52c

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,14 +7,24 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
+    toy = toy_find
     toy.update(toy_params)
+    render json: toy
   end
+
+  def increase_likes
+    toy = toy_find
+    toy.update(likes: + 1)
+    render json: toy
+  end
+
 
   def destroy
     toy = Toy.find_by(id: params[:id])
@@ -26,6 +36,10 @@ class ToysController < ApplicationController
   
   def toy_params
     params.permit(:name, :image, :likes)
+  end
+
+  def toy_find
+    toy = Toy.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :toys, only: [:index, :create, :update]
+  resources :toys, only: [:index, :create, :update, :destroy]
+  get 'toys/:id/likes', to: 'toys#increase_likes'
 end
